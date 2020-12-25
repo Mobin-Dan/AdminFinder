5056b90be75fd4eed4f717e326b859bf52408b9d
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-12-25 13:00:55 +0330

    Create install.py

---
+++ install.py
@@ -0,0 +1,12 @@
+from colorama import Fore
+import os
+os.system("apt update")
+os.system("pkg install python")
+os.system("pkg install python2")
+os.system("pip install -r requirements.txt")
+os.system("pip install colorama")
+os.system("pkg install cowsay")
+os.system("clear")
+os.system("cowsay run script :) |lolcat")
+
+

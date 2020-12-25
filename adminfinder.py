20322408a91e7fa5e698bd683d09136073f47652
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-12-25 12:37:15 +0330

    Create adminfinder.py

---
+++ adminfinder.py
@@ -0,0 +1,29 @@
+import urllib
+from colorama import Fore
+print'''
+            #################################################
+            #################################################
+            ##########  Ashiyane ADMINPAGE FindeR  ##########
+            ############ CreaTed BY Mobin_dan‌‌ ‌ ‌‌    ##########
+            ############  WE LOVE IRAN           ############
+            #################################################
+            #################################################
+
+'''
+
+url=raw_input("Enter Target's WebSite Url (ex:https/http website.com): ")
+try:
+    OF=open("admin.txt","r")
+    for line in OF:
+        url2=url +"/"+ line
+        site=urllib.urlopen(url2)
+        if site.code==200:
+           print(Fore.GREEN+"[+]FOUND "+url2)
+        else:
+           print(Fore.RED+"[-]NOT FOUND "+url2)
+    OF.close()
+except KeyboardInterrupt:
+   print(Fore.GREEN)
+   print "Bye Bye !!!"
+   print "t.me/@World_warning"
+
